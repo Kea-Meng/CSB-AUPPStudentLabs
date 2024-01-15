9ef7f070dca6be9bc5580a7730d366fae8737497
KeaMeng-ING <2023484ing@aupp.edu.kh>
2024-01-15 15:18:06 +0700

    Update schoolfilesystem.py

--- schoolfilesystem.py
+++ schoolfilesystem.py
@@ -1,20 +1,38 @@
 
 #Libraries you may need:
-import csv, collections, dictionary, (pandas as pd), urlopen, etc..
+import pandas as pd
+from io import StringIO
+from bs4 import BeautifulSoup
+import requests
 
 #classes and Functions to implement
 class SchoolAssessmentSystem:
     def __init__(self):
-        pass
-    def process_file():            
+        self.data = pd.DataFrame()
+
+    def process_file(self, file_path):
+        if file_path.endswith('.csv'):
+            self.data = pd.read_csv(file_path)
+        elif file_path.endswith('.xlsx'):
+            self.data = pd.read_excel(file_path)
+        elif file_path.endswith('.txt'):
+            with open(file_path, 'r') as file:
+                self.data = pd.read_csv(StringIO(file.read()), delimiter='\t')
 
     def transfer_data():
+        pass
 
     def fetch_web_data():
+        pass
 
     def analyze_content():
+        pass
 
     def generate_summary():
+        pass
 
 
 # Analyze content & display result area
+school = SchoolAssessmentSystem()
+school.process_file("birthdays.csv")
+print(school.data)
